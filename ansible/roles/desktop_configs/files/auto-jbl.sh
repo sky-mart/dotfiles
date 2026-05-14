965f73f4190edb426d262039c4854870aa0cd45a
Vlad <sky-mart@hotmail.com>
2026-05-14 19:49:41 +0200

    Fix the auto-jbl to work on startup as well

--- ansible/roles/desktop_configs/files/auto-jbl.sh
+++ ansible/roles/desktop_configs/files/auto-jbl.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
-dbus-monitor --session "type='signal',interface='org.freedesktop.ScreenSaver'" |
+~/.local/bin/blconnect.sh jbl  # run once at startup
+
+gdbus monitor --session --dest org.freedesktop.ScreenSaver |
 while read -r line; do
-    if [[ "$line" == *"boolean false"* ]]; then
+    if [[ "$line" == *"ActiveChanged"* && "$line" == *"false"* ]]; then
         ~/.local/bin/blconnect.sh jbl
     fi
 done
