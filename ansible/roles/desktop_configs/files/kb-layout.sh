51b241bdb3dc6a555674b713eac53cf99e317fe3
Vlad <sky-mart@hotmail.com>
2026-05-14 14:50:57 +0200

    Make faster switch of keyboard layouts

--- ansible/roles/desktop_configs/files/kb-layout.sh
+++ ansible/roles/desktop_configs/files/kb-layout.sh
@@ -1,32 +1,16 @@
 #!/usr/bin/env bash
 
-STATE_FILE="$HOME/.cache/kb-last-layout"
-# my layouts are us,ru,de
+OPTIONS="grp:alt_shift_toggle,ctrl:swapcaps"
 
 case "$1" in
-    next)
-        current=$(xkb-switch)
-        next=$(xkb-switch -n)
-        echo "$current" > "$STATE_FILE"
-        ;;
-    last)
-        if [[ -f "$STATE_FILE" ]]; then
-            last=$(cat "$STATE_FILE")
-            current=$(xkb-switch)
-            xkb-switch -s "$last"
-            echo "$current" > "$STATE_FILE"
-        fi
+    init|eng)
+        setxkbmap -layout us,ru -option "${OPTIONS}"
         ;;
     ger)
-        echo "ru" > "$STATE_FILE"
-        xkb-switch -s "de"
-        ;;
-    eng)
-        echo "ru" > "$STATE_FILE"
-        xkb-switch -s "us"
+        setxkbmap -layout de,ru -option "${OPTIONS}"
         ;;
     *)
-        echo "Use: next | last"
+        echo "Use: init | ger | eng"
         exit 1
         ;;
 esac
