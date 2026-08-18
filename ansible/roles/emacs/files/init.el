3e275af7779e3c2801f4b37830bca36f57dbe3be
Vlad Agievich <uladzislau.ahiyevich@toolsforhumanity.com>
2026-08-18 11:28:37 +0200

    Fix the emacs 31 face issues

--- ansible/roles/emacs/files/init.el
+++ ansible/roles/emacs/files/init.el
@@ -258,6 +258,10 @@
    'gnus-group-news-low
    '((t (:inherit gnus-group-mail-1 :foreground "#4C566A" :weight bold)))
    'face-override-spec)
+  (face-spec-set
+   'gnus-group-news-low-empty
+   '((t (:inherit gnus-group-mail-1 :foreground "#4C566A" :weight bold)))
+   'face-override-spec)
   ;; Enable flashing mode-line on errors
   (doom-themes-visual-bell-config)
   ;; Corrects (and improves) org-mode's native fontification.
